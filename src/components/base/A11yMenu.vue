b1e9a4d0e3f35b1a4b13a05b6e128ca441b235bc
ciretorres <erictorres.velasco@gmail.com>
2023-01-10 13:17:49 -0600

    primer avance cambio de tipografía atkinson

--- src/components/base/A11yMenu.vue
+++ src/components/base/A11yMenu.vue
@@ -43,6 +43,10 @@
         </span>
         <span class="nombre">Enlaces subrayados</span>
       </button>
+      <button class="a11y-opcion" type="button" @click="cambiarTipografiaAtkinson">
+        <span class="icono"><img src="@/assets/img/base/a11y-restablecer.png" alt=""></span>
+        <span class="nombre">Cambiar tipografia</span>
+      </button>
       <button class="a11y-opcion" type="button" @click="restablecerEstilo">
         <span class="icono"><img src="@/assets/img/base/a11y-restablecer.png" alt=""></span>
         <span class="nombre">Restablecer</span>
@@ -78,7 +82,11 @@ export default {
     alternarEnlacesSubrayados() {
       this.$store.commit('alternarEnlacesSubrayados');
     },
+    cambiarTipografiaAtkinson() {
+      this.$store.commit('cambiarTipografiaAtkinson');
+    },
     restablecerEstilo() {
+      this.$store.commit('quitarCambioDeTipografia');
       this.$store.commit('quitarAltoContraste');
       this.$store.commit('quitarVistaSimplificada');
       this.$store.commit('quitarEnlacesSubrayados');
